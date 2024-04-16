49314379a8eda1ff91d3b2b436a2012ba07e6d9b
Chimbori <hello@chimbori.com>
2024-04-15 17:54:12 -0700

    Convert AboutFragment to use View Binding & clean up the UI in the process

--- app/src/main/java/com/chimbori/catnap/AboutFragment.kt
+++ app/src/main/java/com/chimbori/catnap/AboutFragment.kt
@@ -1,30 +1,23 @@
 package com.chimbori.catnap
 
-import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.chimbori.catnap.FragmentIndex.ID_ABOUT
+import com.chimbori.catnap.databinding.FragmentAboutBinding
+import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 
-class AboutFragment : Fragment() {
-  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-    val v = inflater.inflate(R.layout.about_fragment, container, false)
-    val packageInfo = try {
-      val context: Context? = activity
-      context!!.packageManager.getPackageInfo(context.packageName, 0)
+class AboutFragment : Fragment(R.layout.fragment_about) {
+  private val binding by viewBinding(FragmentAboutBinding::bind)
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    binding.fragmentAboutVersion.text = try {
+      requireContext().packageManager.getPackageInfo(requireActivity().packageName, 0)
     } catch (e: PackageManager.NameNotFoundException) {
-      throw RuntimeException("Can't find package?")
-    }
-
-    // Evaluate the format string in VersionText.
-    val versionText = v.findViewById<View>(R.id.VersionText) as TextView
-    val versionFormat = versionText.getText().toString()
-    versionText.text = String.format(versionFormat, packageInfo.versionName)
-    return v
+      null
+    }?.versionName ?: "__"
   }
 
   override fun onResume() {
