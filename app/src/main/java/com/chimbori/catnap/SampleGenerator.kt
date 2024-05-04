341527a66a60a3fad5977f065e62189ae6b8603b
hello@chimbori.com <Chimbori>
2024-05-03 23:02:27 -0700

    Cleanup `SampleGenerator` and `SampleGeneratorState`

--- app/src/main/java/com/chimbori/catnap/SampleGenerator.kt
+++ app/src/main/java/com/chimbori/catnap/SampleGenerator.kt
@@ -1,58 +1,61 @@
 package com.chimbori.catnap
 
 import android.os.Process
+import android.os.Process.THREAD_PRIORITY_BACKGROUND
 import android.os.SystemClock
 import org.jtransforms.dct.FloatDCT_1D
 
-internal class SampleGenerator(private val mNoiseService: NoiseService, private val mSampleShuffler: SampleShuffler) {
-  private val mWorkerThread: Thread
-  private val mRandom = XORShiftRandom() // Not thread safe.
-
-  // Communication variables; must be synchronized.
-  private var mStopping = false
-  private var mPendingSpectrum: SpectrumData? = null
-
-  // Variables accessed from the thread only.
-  private var mLastDctSize = -1
-  private var mDct: FloatDCT_1D? = null
-
-  init {
-    mWorkerThread = object : Thread("SampleGeneratorThread") {
-      override fun run() {
-        try {
-          threadLoop()
-        } catch (e: StopException) {
-        }
+internal class SampleGenerator(private val noiseService: NoiseService, private val sampleShuffler: SampleShuffler) {
+  private val workerThread = object : Thread("SampleGeneratorThread") {
+    override fun run() {
+      try {
+        threadLoop()
+      } catch (e: StopException) {
+        // Ignore
       }
     }
-    mWorkerThread.start()
+  }
+
+  private val xorShiftRandom = XORShiftRandom() // Not thread safe.
+
+  // Communication variables; must be synchronized.
+  private var isStopping = false
+  private var pendingSpectrum: SpectrumData? = null
+
+  // Variables accessed from the thread only.
+  private var lastDctSize = -1
+  private var dct: FloatDCT_1D? = null
+
+  init {
+    workerThread.start()
   }
 
   fun stopThread() {
     synchronized(this) {
-      mStopping = true
+      isStopping = true
       (this as Object).notify()
     }
     try {
-      mWorkerThread.join()
+      workerThread.join()
     } catch (e: InterruptedException) {
+      // Ignore
     }
   }
 
   @Synchronized
   fun updateSpectrum(spectrum: SpectrumData?) {
-    mPendingSpectrum = spectrum
+    pendingSpectrum = spectrum
     (this as Object).notify()
   }
 
-  @Throws(StopException::class)
   private fun threadLoop() {
-    Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND)
+    Process.setThreadPriority(THREAD_PRIORITY_BACKGROUND)
 
     // Chunk-making progress:
     val state = SampleGeneratorState()
     var spectrum: SpectrumData? = null
     var waitMs: Long = -1
+
     while (true) {
       // This does one of 3 things:
       // - Throw StopException if stopThread() was called.
@@ -62,33 +65,29 @@ internal class SampleGenerator(private val mNoiseService: NoiseService, private 
       if (newSpectrum != null && !newSpectrum.sameSpectrum(spectrum)) {
         spectrum = newSpectrum
         state.reset()
-        mNoiseService.updatePercentAsync(state.percent)
+        noiseService.updatePercentAsync(state.percent)
       } else if (waitMs == -1L) {
-        // Nothing changed.  Keep waiting.
+        // Nothing changed. Keep waiting.
         continue
       }
       val startMs = SystemClock.elapsedRealtime()
 
       // Generate the next chunk of sound.
-      val dctData = doIDCT(state.chunkSize, spectrum)
-      if (mSampleShuffler.handleChunk(dctData, state.stage)) {
+      val dctData = doIDCT(state.chunkSize, spectrum!!)
+      if (sampleShuffler.handleChunk(dctData, state.stage)) {
         // Not dropped.
         state.advance()
-        mNoiseService.updatePercentAsync(state.percent)
+        noiseService.updatePercentAsync(state.percent)
       }
 
-      // Avoid burning the CPU while the user is scrubbing.  For the
-      // first couple large chunks, the next chunk should be ready
-      // when this one is ~75% finished playing.
+      // Avoid burning the CPU while the user is scrubbing. For the first couple large chunks, the next chunk
+      // should be ready when this one is ~75% finished playing.
       val sleepTargetMs = state.getSleepTargetMs(SAMPLE_RATE)
       val elapsedMs = SystemClock.elapsedRealtime() - startMs
-      waitMs = sleepTargetMs - elapsedMs
-      if (waitMs < 0) waitMs = 0
-      if (waitMs > sleepTargetMs) waitMs = sleepTargetMs
-      if (state.done()) {
-        // No chunks left; save RAM.
-        mDct = null
-        mLastDctSize = -1
+      waitMs = (sleepTargetMs - elapsedMs).coerceIn(0, sleepTargetMs)
+      if (state.done()) {  // No chunks left; save RAM.
+        dct = null
+        lastDctSize = -1
         waitMs = -1
       }
     }
@@ -97,8 +96,8 @@ internal class SampleGenerator(private val mNoiseService: NoiseService, private 
   @Synchronized
   @Throws(StopException::class)
   private fun popPendingSpectrum(waitMs: Long): SpectrumData? {
-    if (waitMs != 0L && !mStopping && mPendingSpectrum == null) {
-      // Wait once.  The retry loop is in the caller.
+    if (waitMs != 0L && !isStopping && pendingSpectrum == null) {
+      // Wait once. The retry loop is in the caller.
       try {
         if (waitMs < 0) {
           (this as Object).wait()
@@ -106,36 +105,37 @@ internal class SampleGenerator(private val mNoiseService: NoiseService, private 
           (this as Object).wait(waitMs)
         }
       } catch (e: InterruptedException) {
+        // Ignore
       }
     }
-    if (mStopping) {
+    if (isStopping) {
       throw StopException()
     }
     return try {
-      mPendingSpectrum
+      pendingSpectrum
     } finally {
-      mPendingSpectrum = null
+      pendingSpectrum = null
     }
   }
 
-  private fun doIDCT(dctSize: Int, spectrum: SpectrumData?): FloatArray {
-    if (dctSize != mLastDctSize) {
-      mDct = FloatDCT_1D(dctSize.toLong())
-      mLastDctSize = dctSize
+  private fun doIDCT(dctSize: Int, spectrum: SpectrumData): FloatArray {
+    if (dctSize != lastDctSize) {
+      dct = FloatDCT_1D(dctSize.toLong())
+      lastDctSize = dctSize
     }
     val dctData = FloatArray(dctSize)
-    spectrum!!.fill(dctData, SAMPLE_RATE)
+    spectrum.fill(dctData, SAMPLE_RATE)
 
     // Multiply by a block of white noise.
     var i = 0
     while (i < dctSize) {
-      var rand = mRandom.nextLong()
+      var rand = xorShiftRandom.nextLong()
       for (b in 0..7) {
         dctData[i++] *= rand.toByte() / 128f
         rand = rand shr 8
       }
     }
-    mDct!!.inverse(dctData, false)
+    dct!!.inverse(dctData, false)
     return dctData
   }
 
--- app/src/main/java/com/chimbori/catnap/SampleGeneratorState.kt
+++ app/src/main/java/com/chimbori/catnap/SampleGeneratorState.kt
@@ -1,50 +1,42 @@
 package com.chimbori.catnap
 
 internal class SampleGeneratorState {
-  // Begin in the "done" state.
-  private var mChunkNumber = N_TOTAL_CHUNKS
+  private var chunkNumber = N_TOTAL_CHUNKS  // Begin in the "done" state.
+
   fun reset() {
-    mChunkNumber = 0
+    chunkNumber = 0
   }
 
   fun advance() {
-    mChunkNumber++
+    chunkNumber++
   }
 
-  fun done(): Boolean {
-    return mChunkNumber >= N_TOTAL_CHUNKS
-  }
+  fun done() = chunkNumber >= N_TOTAL_CHUNKS
 
   val stage: Int
-    get() = if (mChunkNumber < N_SMALL_CHUNKS) {
-      // Small chunk.
-      when (mChunkNumber) {
+    get() = when {
+      chunkNumber < N_SMALL_CHUNKS -> when (chunkNumber) {  // Small chunk.
         0 -> S_FIRST_SMALL
         else -> S_OTHER_SMALL
       }
-    } else if (mChunkNumber < N_SMALL_CHUNKS + N_VOLUME_CHUNKS) {
-      // Large chunk, with volume computation.
-      when (mChunkNumber) {
+      chunkNumber < N_SMALL_CHUNKS + N_VOLUME_CHUNKS -> when (chunkNumber) {  // Large chunk, with volume computation.
         N_SMALL_CHUNKS -> S_FIRST_VOLUME
         N_SMALL_CHUNKS + N_VOLUME_CHUNKS - 1 -> S_LAST_VOLUME
         else -> S_OTHER_VOLUME
       }
-    } else {
-      // Large chunk, volume already set.
-      S_LARGE_NOCLIP
+      else -> S_LARGE_NOCLIP  // Large chunk, volume already set.
     }
+
   val percent: Int
-    get() = mChunkNumber * 100 / N_TOTAL_CHUNKS
+    get() = chunkNumber * 100 / N_TOTAL_CHUNKS
+
   val chunkSize: Int
-    get() = if (mChunkNumber < N_SMALL_CHUNKS) SMALL_CHUNK_SIZE else LARGE_CHUNK_SIZE
+    get() = if (chunkNumber < N_SMALL_CHUNKS) SMALL_CHUNK_SIZE else LARGE_CHUNK_SIZE
 
   // For the first couple large chunks, returns 75% of the chunk duration.
-  fun getSleepTargetMs(sampleRate: Int): Long {
-    val i = mChunkNumber - N_SMALL_CHUNKS
-    return if (0 <= i && i < 2) {
-      (750 * LARGE_CHUNK_SIZE / sampleRate).toLong()
-    } else 0
-  }
+  fun getSleepTargetMs(sampleRate: Int) = if (chunkNumber - N_SMALL_CHUNKS in 0..1) {
+    (750 * LARGE_CHUNK_SIZE / sampleRate).toLong()
+  } else 0
 
   companion object {
     // List of possible chunk stages.
@@ -55,20 +47,22 @@ internal class SampleGeneratorState {
     const val S_LAST_VOLUME = 4
     const val S_LARGE_NOCLIP = 5
 
-    // How many small preview chunks to generate at first.
+    /** How many small preview chunks to generate at first. */
     private const val N_SMALL_CHUNKS = 4
 
-    // How many final full-size chunks to generate.
+    /** How many final full-size chunks to generate. */
     private const val N_LARGE_CHUNKS = 20
 
-    // How many chunks overall.
+    /** How many chunks overall. */
     private const val N_TOTAL_CHUNKS = N_SMALL_CHUNKS + N_LARGE_CHUNKS
 
-    // How many large chunks to use for estimating the global volume.
+    /** How many large chunks to use for estimating the global volume. */
     private const val N_VOLUME_CHUNKS = 4
 
-    // Size of small/large chunks, in samples.
+    /** Size of small chunk, in samples. */
     private const val SMALL_CHUNK_SIZE = 8192
+
+    /** Size of large chunk, in samples. */
     private const val LARGE_CHUNK_SIZE = 65536
   }
 }
